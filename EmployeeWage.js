f4a4ca4a393c61c6d6db4adc31f0de9e8b8cfe1c
Diwakar <diwakar.n@tvsnext.io>
2021-08-12 11:21:00 +0530

    [Diwakar] Check whether the employee is present or absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,3 +1,11 @@
 "use strict";
 
-alert("Employee Wage Problem");
+//Uc1
+const ABSENT = 0;
+
+//genrate random value either 0 or 1
+let randomValue = Math.floor(Math.random() *10)%2; 
+
+//check and print the output
+console.log(`Random value = ${randomValue}`);
+console.log((ABSENT==randomValue)?"Employee is Absent":"Employee is present");
